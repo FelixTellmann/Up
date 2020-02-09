cf51a5b9d2bba22760aafb820ff3184c300b7c9e
FelixTellmann <Fr10tz1!1>
2020-02-09 18:51:36 +0200

    Initial Commit..

---
+++ pages/api/component.js
@@ -0,0 +1,10 @@
+const { database } = require('../../lib/database');
+
+export default async (req, res) => {
+  const data = (await database.query(`SELECT *
+                                      FROM component`))[0];
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ data });
+}
